155c147f8e256bcb56d4217f574493b8b0bb7ca5
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-07-24 09:44:20 +0600

    converted to stateful widget, isPassword parameter instead of obscure text

--- lib/aura_widgets/aura_text_field.dart
+++ lib/aura_widgets/aura_text_field.dart
@@ -1,7 +1,7 @@
 import 'package:aurabajar/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
-class AuraTextField extends StatelessWidget {
+class AuraTextField extends StatefulWidget {
   const AuraTextField({
     super.key,
     this.controller,
@@ -15,7 +15,7 @@ class AuraTextField extends StatelessWidget {
     this.suffixIcon,
     this.label,
     this.hint,
-    this.obscureText = false,
+    this.isPassword = false,
     this.enabled = true,
     this.readOnly = false,
     this.maxLines = 1,
@@ -34,7 +34,7 @@ class AuraTextField extends StatelessWidget {
   final Widget? prefixIcon;
   final Widget? suffixIcon;
 
-  final bool obscureText;
+  final bool isPassword;
   final bool enabled;
   final bool readOnly;
 
@@ -46,44 +46,64 @@ class AuraTextField extends StatelessWidget {
   final ValueChanged<String>? onFieldSubmitted;
 
   @override
-  Widget build(BuildContext context) {
-    const radius = BorderRadius.all(Radius.circular(12));
+  State<AuraTextField> createState() => _AuraTextFieldState();
+}
 
+class _AuraTextFieldState extends State<AuraTextField> {
+  late bool _obscureText;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.isPassword;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     OutlineInputBorder border(Color color) {
       return OutlineInputBorder(
-        borderRadius: radius,
+        borderRadius: BorderRadius.circular(12),
         borderSide: BorderSide(color: color, width: 1.2),
       );
     }
 
     return TextFormField(
-      controller: controller,
-      focusNode: focusNode,
-      keyboardType: keyboardType,
-      textInputAction: textInputAction,
-      validator: validator,
-      onChanged: onChanged,
-      onFieldSubmitted: onFieldSubmitted,
-      obscureText: obscureText,
-      enabled: enabled,
-      readOnly: readOnly,
-      maxLines: obscureText ? 1 : maxLines,
-      minLines: minLines,
+      controller: widget.controller,
+      focusNode: widget.focusNode,
+      keyboardType: widget.keyboardType,
+      textInputAction: widget.textInputAction,
+      validator: widget.validator,
+      onChanged: widget.onChanged,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      obscureText: _obscureText,
+      enabled: widget.enabled,
+      readOnly: widget.readOnly,
+      maxLines: widget.isPassword ? 1 : widget.maxLines,
+      minLines: widget.minLines,
       cursorColor: AppColors.primary,
       decoration: InputDecoration(
-        labelText: label,
-        hintText: hint,
+        labelText: widget.label,
+        hintText: widget.hint,
 
-        prefixIcon: prefixIcon,
-        suffixIcon: suffixIcon,
-
+        prefixIcon: widget.prefixIcon,
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                icon: Icon(
+                  _obscureText
+                      ? Icons.visibility_off_outlined
+                      : Icons.visibility_outlined,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _obscureText = !_obscureText;
+                  });
+                },
+              )
+            : widget.suffixIcon,
         filled: true,
         fillColor: AppColors.white,
 
-        contentPadding: const EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: 16,
-        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
 
         border: border(AppColors.grey),
         enabledBorder: border(AppColors.grey),
