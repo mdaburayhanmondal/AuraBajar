a0ea494be1c7acd66fa3bec29698201842805cc8
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-07-24 09:55:44 +0600

    custom clickable text

---
+++ lib/aura_widgets/aura_linked_text.dart
@@ -0,0 +1,23 @@
+import 'package:aurabajar/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class AuraLinkedText extends StatelessWidget {
+  const AuraLinkedText({super.key, required this.text, this.onTap});
+  final String text;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap ?? () {},
+      child: Text(
+        text,
+        style: TextStyle(
+          color: AppColors.primary,
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+        ),
+      ),
+    );
+  }
+}
