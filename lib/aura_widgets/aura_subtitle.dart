7a220a9a5f6b8de5e06c79fc9cda32af13f5aa28
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-07-24 10:03:21 +0600

    added font weight option

--- lib/aura_widgets/aura_subtitle.dart
+++ lib/aura_widgets/aura_subtitle.dart
@@ -2,15 +2,25 @@ import 'package:aurabajar/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class AuraSubtitle extends StatelessWidget {
-  const AuraSubtitle({super.key, required this.subtitle, this.alignment});
+  const AuraSubtitle({
+    super.key,
+    required this.subtitle,
+    this.alignment,
+    this.weight,
+  });
   final String subtitle;
   final TextAlign? alignment;
+  final FontWeight? weight;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       subtitle,
-      style: TextStyle(color: AppColors.grey, fontSize: 15),
+      style: TextStyle(
+        color: AppColors.grey,
+        fontSize: 15,
+        fontWeight: weight ?? FontWeight.normal,
+      ),
       textAlign: alignment ?? TextAlign.start,
     );
   }
