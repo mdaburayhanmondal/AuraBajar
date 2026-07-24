1b9241a4cb29eb92612edd54e2f3868e1c5bfce7
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-07-24 09:21:58 +0600

    custom title widget

---
+++ lib/aura_widgets/aura_title.dart
@@ -0,0 +1,19 @@
+import 'package:aurabajar/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class AuraTitle extends StatelessWidget {
+  const AuraTitle({super.key, required this.title, this.alignment});
+  final String title;
+  final TextAlign? alignment;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(fontSize: 20, color: AppColors.black),
+      textAlign: alignment ?? TextAlign.start,
+      overflow: TextOverflow.ellipsis,
+      maxLines: 1,
+    );
+  }
+}
