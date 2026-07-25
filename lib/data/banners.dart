4febe1dceae5be336a59897cb896c23acfbfbd4f
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-07-25 14:09:46 +0600

    banner images

---
+++ lib/data/banners.dart
@@ -0,0 +1,7 @@
+List<String> banners = [
+  'https://picsum.photos/1600/900?random=1',
+  'https://picsum.photos/1600/900?random=2',
+  'https://picsum.photos/1600/900?random=3',
+  'https://picsum.photos/1600/900?random=4',
+  'https://picsum.photos/1600/900?random=5',
+];
