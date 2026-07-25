a64e9407c67a1c60846af3cf2a67613b5737cc4f
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-07-25 21:33:46 +0600

    products data

---
+++ lib/data/products.dart
@@ -0,0 +1,89 @@
+final List<Map<String, dynamic>> featuredProducts = [
+  {
+    "image": "https://picsum.photos/200?random=1",
+    "name": "Nike Air Max 270",
+    "originalPrice": 149.99,
+    "sellPrice": 119.99,
+  },
+  {
+    "image": "https://picsum.photos/200?random=2",
+    "name": "Sony WH-1000XM5",
+    "originalPrice": 399.99,
+    "sellPrice": 349.99,
+  },
+  {
+    "image": "https://picsum.photos/200?random=3",
+    "name": "Apple Watch Series 9",
+    "originalPrice": 429.99,
+    "sellPrice": 389.99,
+  },
+  {
+    "image": "https://picsum.photos/200?random=4",
+    "name": "MacBook Air M3",
+    "originalPrice": 1299.99,
+    "sellPrice": 1199.99,
+  },
+];
+
+final List<Map<String, dynamic>> allProducts = [
+  {
+    "image": "https://picsum.photos/200?random=1",
+    "name": "Nike Air Max 270",
+    "originalPrice": 149.99,
+    "sellPrice": 119.99,
+  },
+  {
+    "image": "https://picsum.photos/200?random=2",
+    "name": "Sony WH-1000XM5",
+    "originalPrice": 399.99,
+    "sellPrice": 349.99,
+  },
+  {
+    "image": "https://picsum.photos/200?random=3",
+    "name": "Apple Watch Series 9",
+    "originalPrice": 429.99,
+    "sellPrice": 389.99,
+  },
+  {
+    "image": "https://picsum.photos/200?random=4",
+    "name": "MacBook Air M3",
+    "originalPrice": 1299.99,
+    "sellPrice": 1199.99,
+  },
+  {
+    "image": "https://picsum.photos/200?random=5",
+    "name": "Samsung Galaxy S24",
+    "originalPrice": 999.99,
+    "sellPrice": 899.99,
+  },
+  {
+    "image": "https://picsum.photos/200?random=6",
+    "name": "Canon EOS R50",
+    "originalPrice": 879.99,
+    "sellPrice": 799.99,
+  },
+  {
+    "image": "https://picsum.photos/200?random=7",
+    "name": "Travel Backpack 40L",
+    "originalPrice": 89.99,
+    "sellPrice": 64.99,
+  },
+  {
+    "image": "https://picsum.photos/200?random=8",
+    "name": "JBL Flip 6",
+    "originalPrice": 149.99,
+    "sellPrice": 119.99,
+  },
+  {
+    "image": "https://picsum.photos/200?random=9",
+    "name": "iPad Air 11\"",
+    "originalPrice": 699.99,
+    "sellPrice": 649.99,
+  },
+  {
+    "image": "https://picsum.photos/200?random=10",
+    "name": "Logitech MX Master 3S",
+    "originalPrice": 109.99,
+    "sellPrice": 89.99,
+  },
+];
