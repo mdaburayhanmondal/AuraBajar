0cb5c7717b1b481a9b9fb0a2845d4dbcab195879
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-07-27 20:46:09 +0600

    basic appbar done

--- lib/screens/all_products_screen.dart
+++ lib/screens/all_products_screen.dart
@@ -9,10 +9,12 @@ class AllProductsScreen extends StatefulWidget {
 }
 
 class _AllProductsScreenState extends State<AllProductsScreen> {
+  bool gridView = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        forceMaterialTransparency: true,
         title: Text("All Products"),
         actions: [
           InkWell(onTap: () {}, child: Icon(Icons.search, size: 28)),
@@ -27,6 +29,68 @@ class _AllProductsScreenState extends State<AllProductsScreen> {
           ),
           SizedBox(width: 20),
         ],
+        bottom: PreferredSize(
+          preferredSize: Size(MediaQuery.of(context).size.width, 20),
+          child: Padding(
+            padding: EdgeInsetsGeometry.all(4),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 8,
+                    children: [
+                      Icon(Icons.filter_alt_outlined),
+                      Text(
+                        "Filter",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 8,
+                    children: [
+                      Text(
+                        "Sort By",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Icon(Icons.keyboard_arrow_down),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: InkWell(
+                    splashColor: Colors.transparent,
+                    onTap: () {
+                      setState(() {
+                        gridView = !gridView;
+                      });
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 8,
+                      children: [
+                        Icon(gridView ? Icons.grid_view : Icons.list),
+                        Text(
+                          gridView ? "Grid" : "List",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
