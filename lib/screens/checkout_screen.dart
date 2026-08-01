c4514c7a3d9577ed24f2ede6bbb14b685c4bc082
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-08-01 17:02:40 +0600

    checkout screen done

--- lib/screens/checkout_screen.dart
+++ lib/screens/checkout_screen.dart
@@ -1,12 +1,21 @@
+import 'package:aurabajar/aura_widgets/aura_button.dart';
 import 'package:aurabajar/aura_widgets/aura_linked_text.dart';
 import 'package:aurabajar/aura_widgets/aura_subtitle.dart';
 import 'package:aurabajar/aura_widgets/aura_title.dart';
 import 'package:aurabajar/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
-class CheckoutScreen extends StatelessWidget {
+class CheckoutScreen extends StatefulWidget {
   const CheckoutScreen({super.key});
 
+  @override
+  State<CheckoutScreen> createState() => _CheckoutScreenState();
+}
+
+enum PaymentMethod { bkash, nagad, cod, card }
+
+class _CheckoutScreenState extends State<CheckoutScreen> {
+  PaymentMethod _selectedMethod = PaymentMethod.bkash;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +23,7 @@ class CheckoutScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 12),
         child: Column(
+          spacing: 12,
           children: [
             // delivery address
             Container(
@@ -67,6 +77,270 @@ class CheckoutScreen extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+            // payment methods
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: BoxBorder.all(color: AppColors.grey.withAlpha(40)),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                spacing: 12,
+                children: [
+                  Align(
+                    alignment: AlignmentGeometry.centerLeft,
+                    child: AuraTitle(title: "Payment Method"),
+                  ),
+                  RadioGroup(
+                    groupValue: _selectedMethod,
+                    onChanged: (PaymentMethod? value) {
+                      if (value != null) {
+                        setState(() {
+                          _selectedMethod = value;
+                        });
+                      }
+                    },
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Radio(
+                                  value: PaymentMethod.bkash,
+                                  activeColor: AppColors.primary,
+                                ),
+                                Text(
+                                  "bKash",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              width: 40,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    "https://static.vecteezy.com/system/resources/thumbnails/068/764/270/small_2x/bkash-logo-mobile-banking-app-icon-transparent-background-free-png.png",
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Radio(
+                                  value: PaymentMethod.nagad,
+                                  activeColor: AppColors.primary,
+                                ),
+                                Text(
+                                  "Nagad",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              width: 40,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    "https://freelogopng.com/images/all_img/1679248787Nagad-Logo.png",
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Radio(
+                                  value: PaymentMethod.cod,
+                                  activeColor: AppColors.primary,
+                                ),
+                                Text(
+                                  "Cash on Delivery",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Radio(
+                                  value: PaymentMethod.card,
+                                  activeColor: AppColors.primary,
+                                ),
+                                Text(
+                                  "Visa / Master Card",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              spacing: 8,
+                              children: [
+                                Container(
+                                  width: 40,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                        "https://images.seeklogo.com/logo-png/14/2/visa-logo-png_seeklogo-149697.png",
+                                      ),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  width: 40,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Mastercard-logo.svg/3840px-Mastercard-logo.svg.png",
+                                      ),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // calculation
+            Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                border: BoxBorder.all(color: AppColors.grey.withAlpha(40)),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                spacing: 16,
+                children: [
+                  Align(
+                    alignment: AlignmentGeometry.centerLeft,
+                    child: AuraTitle(title: "Order Summary"),
+                  ),
+                  Column(
+                    spacing: 12,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Subtotals (3 items)",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: AppColors.black,
+                            ),
+                          ),
+                          Text(
+                            "৳ 6,597",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: AppColors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Shipping Charge",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: AppColors.black,
+                            ),
+                          ),
+                          Text(
+                            "৳ 60",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: AppColors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Discount",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: AppColors.black,
+                            ),
+                          ),
+                          Text(
+                            "- ৳ 300",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16),
+            Column(
+              spacing: 24,
+              children: [
+                AuraButton(onPressed: () {}, text: "Place Order"),
+                Row(
+                  spacing: 8,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.lock_outline, color: AppColors.grey),
+                    AuraSubtitle(
+                      subtitle: "Your payment information is secured.",
+                    ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
